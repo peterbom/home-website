352c6a26be5c41c2f7d03eaeaf5d17b00d38c1e2
gituser <gituser@bombnas.com>
2016-09-12 14:10:33 +1200

    photo upload functionality

--- src/pages/photos/import.js
+++ src/pages/photos/import.js
@@ -1,12 +1,62 @@
 import {inject, NewInstance} from "aurelia-framework";
 import {Endpoint} from "aurelia-api";
+import {DialogService} from "aurelia-dialog";
 
-@inject(Endpoint.of("main"))
+@inject(Endpoint.of("main"), DialogService)
 export class Import {
-    constructor (endpoint) {
+
+    selectedFiles;
+
+    constructor (endpoint, dialogService) {
         this._endpoint = endpoint;
+        this._dialogService = dialogService;
     }
 
     async activate (params) {
+    }
+
+    async import() {
+        if (!this.selectedFiles || this.selectedFiles.length === 0) {
+            return;
+        }
+
+        let batch = [];
+        let batches = [batch];
+        for (let i = 0; i < this.selectedFiles.length; i++) {
+            batch.push(this.selectedFiles.item(i));
+            if (batch.length === 10) {
+                batch = [];
+                batches.push(batch);
+            }
+        }
+
+        let controller = await this._dialogService.openAndYieldController({
+            viewModel: this.loadingModal,
+            model: {
+                message: "Importing",
+                progressPercent: 0
+            }});
+
+        let upload = await this._endpoint.post("photo-upload");
+
+        let uploadResource = `photo-upload/${upload._id}`;
+
+        for (let batch of batches) {
+            let uploadImage = async file => {
+                await this._endpoint.update(uploadResource, file.name, this.selectedFiles.item(0), {
+                    headers: {
+                        "Content-Type": null // 'application/octet-stream'
+                    }
+                });
+
+                controller.viewModel.progressPercent += (1 / this.selectedFiles.length) * 100;
+            };
+
+            await Promise.all(batch.map(uploadImage));
+        }
+
+        this.selectedFiles = [];
+
+        controller.cancel();
     }
 }
--- src/resources/converters/index.js
+++ src/resources/converters/index.js
@@ -1,6 +1,8 @@
 export function configure(aurelia) {
     aurelia.globalResources(
         "./auth-filter-value-converter",
+        "./blob-to-url-value-converter",
         "./data-uri-value-converter",
-        "./date-time-format-value-converter");
+        "./date-time-format-value-converter",
+        "./file-list-to-array-value-converter");
 }
